4753f29d92e844a28b2ee19ace2dfa9543ccab30
liam <liam@localhost.localdomain>
2017-01-02 01:20:49 +0800

    1

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#这个#!字符序列是一种特殊的结构叫做 shebang。 这个 shebang 被用来告诉操作系统将执行此脚本所用的解释器的名字。每个 shell 脚本都应该把这一文本行 作为它的第一行。
+
+# Bunch of symlinks
+
+ln -sfv ~/dotfile/vim/.vimrc ~
---
+++ vim/.vimrc
@@ -0,0 +1,4 @@
+set number "添加行号
+set autoindent
+set tabstop=4 "tab4个空格
+set expandtab
