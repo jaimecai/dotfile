3e4f4cef6f86a7c2aca132b6cda4c4c9e01ceb44
Jaime Cai <1181929342@qq.com>
2017-07-19 23:32:15 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 #此行称为 shebang（就是 sharp (#) + bang (!) 的意思），会指引操作系统使用接下来指定的程序运行此文件。此处 /bin/bash 执行我们的文件。
 
 yum install vim
+yum install shadowsocks
+ssserver -p 2501 -k jaimecai -m aes-256-cfb --user nobody -d start
 #设置install.sh的目录的全局环境变量 让脚本可以在任意地方运行
 export DOTFILES_DIR
 DOTFILES_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
